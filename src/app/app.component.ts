33dd33b29ec69c2cd39ff85bcaa10985acca9fa4
c0dem0nkey <johnsmithdoe@github.com>
2024-02-25 08:38:41 +0100

    NP-11: Native share and barcode scanning tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import {JsonPipe} from "@angular/common";
-import {Component, OnInit} from '@angular/core';
-import {BarcodeScanner} from '@capacitor-mlkit/barcode-scanning';
+import {Component, inject, OnInit} from '@angular/core';
+import {Barcode, BarcodeFormat, BarcodeScanner} from '@capacitor-mlkit/barcode-scanning';
+import {Share} from "@capacitor/share";
 import {
+  AlertController,
   IonApp,
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
@@ -17,16 +20,82 @@ import {
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, JsonPipe],
+  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, JsonPipe, IonButton],
 })
 export class AppComponent implements OnInit {
-  isSupported = false;
-  constructor() {}
+  readonly #alertController = inject(AlertController);
 
-  ngOnInit(): void {
-    BarcodeScanner.isSupported().then((result) => {
-      this.isSupported = result.supported;
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+
+  async ngOnInit() {
+    this.isSupported = (await BarcodeScanner.isSupported()).supported;
+  }
+
+  async isGoogleBarcodeScannerModuleAvailable() {
+    const {available} =
+      await BarcodeScanner.isGoogleBarcodeScannerModuleAvailable();
+    return available;
+  };
+
+  async installGoogleBarcodeScannerModule() {
+    BarcodeScanner.addListener('googleBarcodeScannerModuleInstallProgress', (ev) => {
+      console.log(ev.progress, ev.state);
+      if (ev.progress ?? 0 >= 100) {
+        console.log('did install module now scanning');
+        document.querySelector('body')?.classList.add('scanner-active');
+        BarcodeScanner.scan().then(result => {
+          console.log(result);
+          this.barcodes.push(...result.barcodes);
+        }).catch(err => console.error(err));
+      }
+    });
+    await BarcodeScanner.installGoogleBarcodeScannerModule();
+  };
+
+
+  async scan(): Promise<void> {
+    if (this.isSupported) {
+      console.log('check permissions');
+      const granted = await this.#requestPermissions();
+      if (!granted) {
+        await this.#presentAlert();
+        return;
+      }
+      console.log(this.isSupported);
+      const hasModule = await this.isGoogleBarcodeScannerModuleAvailable();
+      console.log(hasModule, 'hase mod but starting it');
+      document.querySelector('body')?.classList.add('scanner-active');
+      await BarcodeScanner.startScan({formats: [BarcodeFormat.Ean13]});
+      // if (!hasModule) {
+      //   console.log(hasModule, 'installing module');
+      //   await this.installGoogleBarcodeScannerModule();
+      //   console.log('after await');
+      //
+      // }
+    }
+  }
+
+  async shareShoppingList() {
+    await Share.share({
+      title: 'See cool stuff',
+      text: 'Really awesome thing you need to see right meow',
+      dialogTitle: 'Share with buddies',
     });
   }
 
+  async #requestPermissions(): Promise<boolean> {
+    const {camera} = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async #presentAlert(): Promise<void> {
+    const alert = await this.#alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
